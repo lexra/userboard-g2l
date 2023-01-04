982d3bc1bcb6437f17b7963d30f3c334db3afc65
Lexra Chang <jason.chang@digitalmediacartridge.com>
2023-01-04 16:31:10 +0800

    ubuntu-18.04 python3.8

--- meta-userboard-g2l/recipes-demo/app-resnet50-cam/resnet50_cam.sh
+++ meta-userboard-g2l/recipes-demo/app-resnet50-cam/resnet50_cam.sh
@@ -7,9 +7,6 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 [ ! -x ../../../sources/drp-ai_translator_release/run_DRP-AI_translator_V2L.sh ] && exit 1
-[ -x /usr/bin/python3.8 -a -x /usr/bin/x86_64-linux-gnu-python3.8-config ] && sudo ln -sf python3.8 /usr/bin/python3
-[ -x /usr/bin/python3.8 -a -x /usr/bin/x86_64-linux-gnu-python3.8-config ] && sudo ln -sf x86_64-linux-gnu-python3.8-config /usr/bin/python3.8-config
-[ -x /usr/bin/python3.8 -a -x /usr/bin/x86_64-linux-gnu-python3.8-config ] && sudo ln -sf python3.8-config /usr/bin/python3-config
 
 ##########################################################
 APP_NAME=app_resnet50_cam
@@ -17,16 +14,28 @@ APP_RECIPE=$(echo $APP_NAME | sed 's/_/-/g')
 MODEL=resnet50
 IMG_SRC=cam
 
+##########################################################
+NEED_RESTORE=0
+if [ -x /usr/bin/python3.8 -a -x /usr/bin/x86_64-linux-gnu-python3.8-config -a "$(lsb_release -a | grep 'Release:' | awk '{print $2}')" == ""18.04 ]; then
+	echo -e "${YELLOW} => update python3 to python3.8 ${NC}"
+	echo ""
+	sudo ln -sf python3.8 /usr/bin/python3
+	sudo ln -sf x86_64-linux-gnu-python3.8-config /usr/bin/python3.8-config
+	sudo ln -sf python3.8-config /usr/bin/python3-config
+	NEED_RESTORE=1
+fi
+
+##########################################################
 if [ "${MODEL}" == "resnet50" ]; then
 	echo -e "${YELLOW} => ${MODEL}.onnx ${NC}"
 	cd ../../../sources/drp-ai_translator_release/pytorch/${MODEL}
-	rm -rf ${MODEL}.onnx
 	python3 convert_to_onnx.py
-	cp -Rpfv ./${MODEL}.onnx ../../onnx/
+	cp -Rpfv ./${MODEL}.onnx ../../onnx
 	cd -
 fi
 
 ##########################################################
+echo ""
 echo -e "${YELLOW} => DRP-AI Translate ${NC}"
 cd ../../../sources/drp-ai_translator_release
 rm -rf output/${MODEL}_${IMG_SRC}
@@ -35,5 +44,21 @@ rm -rf output/${MODEL}_${IMG_SRC}
 	-prepost ../../meta-userboard-g2l/recipes-demo/${APP_RECIPE}/${APP_NAME}/etc/prepost_${MODEL}.yaml \
 	-addr ../../meta-userboard-g2l/recipes-demo/${APP_RECIPE}/${APP_NAME}/etc/addrmap_in_${MODEL}.yaml
 cd -
+
+##########################################################
+echo ""
+echo -e "${YELLOW} => ${APP_NAME}/exe/${MODEL}_${IMG_SRC} ${NC}"
+rm -rf ${APP_NAME}/exe/${MODEL}_${IMG_SRC}
 /bin/cp -Rpf ../../../sources/drp-ai_translator_release/output/${MODEL}_${IMG_SRC} ${APP_NAME}/exe
-ls -l ${APP_NAME}/exe/${MODEL}_${IMG_SRC}
+ls -l --color ${APP_NAME}/exe/${MODEL}_${IMG_SRC}
+
+##########################################################
+if [ -x /usr/bin/python3.6 -a -x /usr/bin/x86_64-linux-gnu-python3.6-config -a 1 -eq ${NEED_RESTORE} ]; then
+	echo ""
+	echo -e "${YELLOW} => restore python3 to default ${NC}"
+	sudo ln -sf python3.6 /usr/bin/python3
+	sudo ln -sf x86_64-linux-gnu-python3.6-config /usr/bin/python3.6-config
+	sudo ln -sf python3.6-config /usr/bin/python3-config
+fi
+
+exit 0
