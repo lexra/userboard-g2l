5f73b855873ab9cec978687d518ad62578de0421
Lexra Chang <jason.chang@digitalmediacartridge.com>
2023-01-10 14:51:08 +0800

    meta-gnk-board.patch

--- build.sh
+++ build.sh
@@ -184,9 +184,14 @@ git -C meta-clang checkout -b develop e63d6f9abba5348e2183089d6ef5ea384d7ae8d8 |
 echo -e ${GREEN}'>> meta-browser '${NC}
 git clone https://github.com/OSSystems/meta-browser || true
 git -C meta-browser checkout -b develop dcfb4cedc238eee8ed9bd6595bdcacf91c562f67 || true
+
+##########################################################
+cd ${SCRIP_DIR}/sources
 echo -e ${GREEN}'>> meta-gnk-board '${NC}
 git clone https://github.com/xlloss/meta-gnk-board.git || true
 git -C meta-gnk-board checkout -b develop 6ce09ce5733b1765c3547826816fb081fdbd84b7 || true
+git -C meta-gnk-board checkout .
+[ -e patches/meta-gnk-board.patch ] && patch -p1 -l -f --fuzz 3 -d meta-gnk-board -i ../../patches/meta-gnk-board.patch
 sed 's|userboard|gnk|g' -i meta-gnk-board/conf/layer.conf
 sed 's|^BBFILES += "${@|#BBFILES += "${@|g' -i meta-gnk-board/conf/layer.conf
 sed 's|^               for layer in BBFILE_COLLECTIONS.split()|#               for layer in BBFILE_COLLECTIONS.split()|g' -i meta-gnk-board/conf/layer.conf
