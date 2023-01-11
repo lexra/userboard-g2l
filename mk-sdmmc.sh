7d463cdb8bc627c4f34cf52e1fb109c62a3663ce
Lexra Chang <jason.chang@digitalmediacartridge.com>
2023-01-11 10:27:02 +0800

    TOTAL=5120 PART1=1280

--- mk-sdmmc.sh
+++ mk-sdmmc.sh
@@ -20,8 +20,8 @@ BUILD_DIR=build_${TARGET_BOARD}
 [ "${TARGET_BOARD}" == "gnk-rzv2l" ] && SOC_FAMILY=r9a07g054l
 
 SDDEV=${SCRIP_DIR}/build_${TARGET_BOARD}/tmp/deploy/images/${TARGET_BOARD}/SDMMC.img
-TOTAL=5120
-PART1=1280
+TOTAL=8192
+PART1=2560
 
 function print_boot_example() {
 	echo ""
