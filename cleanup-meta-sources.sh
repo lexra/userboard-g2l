1a9dfa60cdadfa320e17f954e34a1f45be2b333a
Lexra Chang <jason.chang@digitalmediacartridge.com>
2023-01-13 11:04:50 +0800

    [ ! -d Renesas_software ] && exit 1

--- cleanup-meta-sources.sh
+++ cleanup-meta-sources.sh
@@ -5,18 +5,7 @@ YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-if [ ! -e Renesas_software/RTK0EF0045Z0024AZJ-v3.0.0-update2.zip ]; then
-        echo -e ${YELLOW}'Please download the RTK0EF0045Z0024AZJ-v3.0.0-update2.zip from renesas.com . '${NC}
-        exit 1
-fi
-if [ ! -e Renesas_software/RTK0EF0045Z13001ZJ-v1.21_EN.zip ]; then
-        echo -e ${YELLOW}'Please download the RTK0EF0045Z13001ZJ-v1.21_EN.zip from renesas.com . '${NC}
-        exit 1
-fi
-if [ ! -e Renesas_software/RTK0EF0045Z15001ZJ-v0.58_EN.zip ]; then
-        echo -e ${YELLOW}'Please download the RTK0EF0045Z15001ZJ-v0.58_EN.zip from renesas.com . '${NC}
-        exit 1
-fi
+[ ! -d Renesas_software ] && exit 1
 
 cd Renesas_software
 find . -type d | xargs rm -rfv || true
